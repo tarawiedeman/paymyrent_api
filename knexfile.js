114955d0867d8fba9e2e91e1b53d36f88bd6a825
tarawiedeman <33428501+tarawiedeman@users.noreply.github.com>
2023-05-24 12:37:34 -0700

    updated knex server info for deployment

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,18 @@ require("dotenv").config();
 module.exports = {
   client: "mysql",
   connection: {
-    host: "127.0.0.1",
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
-    database: process.env.DB_LOCAL_DBNAME,
+    host: process.env.JAWSDB_HOST,
+    port:process.env.JAWSDB_PORT,
+    user: process.env.JAWSDB_USER,
+    password: process.env.JAWSDB_PASSWORD,
+    database: process.env.JAWSDB_DB,
   },
 };
+
+
+// connection: {
+//   host: "127.0.0.1",
+//   user: process.env.DB_LOCAL_USER,
+//   password: process.env.DB_LOCAL_PASSWORD,
+//   database: process.env.DB_LOCAL_DBNAME,
+// },
